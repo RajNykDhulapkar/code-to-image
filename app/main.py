f6f1c0e378b6bb34f3e23871e1acb8f036a7c438
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-11-03 19:39:01 +0530

    added route with query parameters route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, APIRouter
 
+from typing import Optional
+
 app = FastAPI(
     title="Recipe API", openapi_url="/openapi.json"
 )
@@ -46,6 +48,26 @@ async def fetch_recipe(*, recipe_id: int) -> dict:
     result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
     if result:
         return result[0]
+
+
+@api_router.get("/search/", status_code=200)
+async def search_recipes(
+        keyword: Optional[str] = None, max_results: Optional[int] = 10
+) -> dict:
+    """search for recipes using query parameters
+
+    Args:
+        keyword (Optional[str], optional): keyword in recipe label. Defaults to None.
+        max_results (Optional[int], optional): limit for results find query. Defaults to 10.
+
+    Returns:
+        dict: dict with key results containing matched recipes
+    """
+    results = filter(lambda recipe: keyword.lower()
+                     in recipe["label"].lower(), RECIPES)
+    return {
+        "results": list(results)[:max_results]
+    }
 
 
 app.include_router(api_router)
