6c36790519f31a3408f957ed1981abce639a3354
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-11-03 12:57:01 +0530

    added path parameter route

--- app/main.py
+++ app/main.py
@@ -6,10 +6,46 @@ app = FastAPI(
 
 api_router = APIRouter()
 
+RECIPES = [
+    {
+        "id": 1,
+        "label": "Chicken Vesuvio",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/12/chicken-vesuvio-recipe.html",
+    },
+    {
+        "id": 2,
+        "label": "Chicken Paprikash",
+        "source": "No Recipes",
+        "url": "http://norecipes.com/recipe/chicken-paprikash/",
+    },
+    {
+        "id": 3,
+        "label": "Cauliflower and Tofu Curry Recipe",
+        "source": "Serious Eats",
+        "url": "http://www.seriouseats.com/recipes/2011/02/cauliflower-and-tofu-curry-recipe.html",
+    },
+]
+
 
 @api_router.get("/", status_code=200)
 async def root() -> dict:
     return {"message": "deployment ready"}
+
+
+@api_router.get("/recipe/{recipe_id}", status_code=200)
+async def fetch_recipe(*, recipe_id: int) -> dict:
+    """Fetch single recipe by id
+
+    Args:
+        recipe_id (int): recipe id
+
+    Returns:
+        dict: recipe
+    """
+    result = [recipe for recipe in RECIPES if recipe["id"] == recipe_id]
+    if result:
+        return result[0]
 
 
 app.include_router(api_router)
